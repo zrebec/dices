3d4ad55df1218d2a095b9ee38179cd12ab60467f
zrebec <zrebec@zrebec.sk>
2026-03-28 13:14:02 +0100

    fix: vite configuration fix for github actions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	root: 'src',
 	base: '/dices/',
+	server: {
+		port: 3000,
+		open: true,
+	},
+	build: {
+		outDir: '../dist',
+	},
 });
