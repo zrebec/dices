e29aae83d602fce23cf33b9ccabb32d1b4b7562a
zrebec <zrebec@zrebec.sk>
2026-03-28 13:04:18 +0100

    feat: github actions

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: { 
-    outDir: '../dist',
-  }
+	base: '/dices/',
 });
